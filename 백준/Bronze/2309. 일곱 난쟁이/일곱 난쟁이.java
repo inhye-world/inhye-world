4c42bb7ef4dc3f3d045abfdebcd39abfdef797a7
Inhyeeee <51586135+inhye-world@users.noreply.github.com>
2022-03-22 18:14:27 +0900

    [Bronze II] Title: 일곱 난쟁이, Time: 200 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int sum = 0;
+        int spy1 = 0;
+        int spy2 = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length-1 ; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    spy1 = i;
+                    spy2 = j;
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(i == spy1 || i ==spy2){
+                continue;
+            }
+            System.out.println(arr[i]);
+        }
+    }
+}
