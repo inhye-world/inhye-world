9c769c73aec8a1f80405e11399aa0c8a0561df9e
Inhyeeee <51586135+inhye-world@users.noreply.github.com>
2022-04-20 11:09:00 +0900

    [level 2] Title: 문자열 압축, Time: 11.96 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -4,52 +4,50 @@ class Solution {
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
         
-        StringBuilder sb = new StringBuilder();
+        //반절 이하여야만 반복하니까
+        int part = s.length()/2;
         
-        //자를 문자열이 반절이상이면 반복할 수 없음
-        int maxp = s.length()/2;
+        String temp;
+        String make;
+        int cnt = 0;
         
         if(s.length() == 1){
-            answer  = 1;
+            return 1;
         }
         
-        for(int i = 1; i <= maxp; i++){
-            String makestr = "";
-            String cur = "";
-            
-            //문자열은 무조건 1개 이상
-            int cnt = 1;
-            
+        for(int i = 1; i <= part; i++){
+            temp = "";
+            make = "";
+            cnt = 1;
             for(int j = 0; j < s.length()/i; j++){
-                //저장해둔 문자열과 다음 자른 문자열이 동일한지 확인
-                if(cur.equals(s.substring(j*i, (j*i)+i))){
+                if(temp.equals(s.substring(i*j, (i*j)+i))){
                     cnt++;
                     continue;
                 }
                 
                 if(cnt > 1){
-                    //문자열 붙임
-                    makestr += cnt+cur;
-                    cnt = 1;
+                    make += cnt + temp;
+                    
                 }else{
-                    makestr += cur;
+                    make += temp;
                 }
-                cur = s.substring(i*j, (i*j)+i);
-            }
-            if(cnt > 1){
-                makestr += cnt+cur;
+                
                 cnt = 1;
+                temp = s.substring(i*j, (i*j)+i);
+            }
+            
+            if(cnt > 1){
+                make += cnt + temp;
             }else{
-                makestr += cur;
+                make += temp;
             }
-        
-            //i로 정확히 나눠 떨어지지 않는 경우
-            if(s.length() % i != 0){
-                makestr += s.substring(s.length()-s.length()%i, s.length());
+            
+            if(s.length()%i != 0){
+                make += s.substring(s.length()-s.length()%i, s.length());
             }
-            answer = answer > makestr.length() ? makestr.length() : answer; 
+            
+            answer = answer > make.length() ? make.length() : answer;
         }
-        
         
         return answer;
     }
